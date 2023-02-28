721b453bacca859ce482cd40b0b1149064e5caef
KaleemAhmed123 <shaamidreez@gmail.com>
2023-02-28 16:19:36 +0530

    All files and setup done.

---
+++ frontend/src/components/index.js
@@ -0,0 +1,9 @@
+export { default as Nav } from "./Nav";
+export { default as leftBar } from "./leftBar";
+export { default as Feed } from "./Feed";
+export { default as PinDetail } from "./PinDetail";
+export { default as UserProfile } from "./UserProfile";
+export { default as CreatePin } from "./CreatePin";
+export { default as Search } from "./Search";
+export { default as Login } from "./Login";
+export { default as MasonryLayout } from "./MasonryLayout";
